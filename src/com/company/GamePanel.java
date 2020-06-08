04c6184846d1ab463ad13e18785e0902c7b81c88
LeeJeongSeok <ljs0429777@gmail.com>
2020-06-08 21:58:01 +0900

    add : 이미지 파일 및 관련 코드 추가

--- src/com/company/GamePanel.java
+++ src/com/company/GamePanel.java
@@ -9,6 +9,19 @@ public class GamePanel extends JPanel implements KeyListener {
 
     private final int B_WIDTH = 300;
     private final int B_HEIGHT = 300;
+    private final int ALL_DOTS = 900;
+
+    private final int x[] = new int[ALL_DOTS];
+    private final int y[] = new int[ALL_DOTS];
+
+    private Image head;
+    private Image tail;
+    private Image apple;
+
+    private int dots;
+    private int apple_x;
+    private int apple_y;
+
 
     public GamePanel() {
         initBoard();
@@ -22,16 +35,53 @@ public class GamePanel extends JPanel implements KeyListener {
 
         setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
         loadImages();
+        initGame();
 
     }
 
     private void loadImages() {
 
+        ImageIcon ihead = new ImageIcon("src/resources/head.png");
+        head = ihead.getImage();
+
+        ImageIcon itail = new ImageIcon("src/resources/dot.png");
+        tail = itail.getImage();
+
+        ImageIcon iapple = new ImageIcon("src/resources/apple.png");
+        apple = iapple.getImage();
     }
 
-    public void paint(Graphics g) {
+    private void initGame() {
+
+        dots = 3;
+
+        for (int z = 0; z < dots; z++) {
+            x[z] = 50 - z * 10;
+            y[z] = 50;
+        }
+
+        locateApple();
 
     }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.drawImage(head, 150, 100, this);
+        g.drawImage(tail, 200, 100, this);
+
+        g.drawImage(apple, apple_x, apple_y, this);
+    }
+
+    // 위치값만 설정
+    private void locateApple() {
+
+        apple_x = 50;
+        apple_y = 50;
+    }
+
+
     @Override
     public void keyTyped(KeyEvent e) {
 
